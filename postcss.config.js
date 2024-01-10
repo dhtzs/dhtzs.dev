386f54c9247a7730ca8c8a5a5eb112d21a6b7730
Dimitris Chatzis <12586834+dhtzs@users.noreply.github.com>
2024-01-10 15:52:13 +0200

    Add PostCSS asset processing

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: {
+      ...(process.env.HUGO_ENVIRONMENT === "production"
+          ? {
+              "autoprefixer": {},
+              "cssnano": {
+                  "preset": ["default", {
+                      "discardComments": false,
+                      "cssDeclarationSorter": false,
+                      "colormin": false
+                  }]
+              }
+          } : {}
+      )
+  }
+};
