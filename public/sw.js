b46e54ba19e0a7e328efa78982bf03d4ea7a33c0
Dimitris Chatzis <12586834+dhtzs@users.noreply.github.com>
2024-01-10 04:14:26 +0200

    Minor fixes

--- public/sw.js
+++ public/sw.js
@@ -1,35 +1,39 @@
-var cacheName = "cache_v2",
-	urlsToCache = [
-		"/",
-		"about-me",
-		"write-ups",
-		"site.webmanifest",
-		"assets/images/profile.jpg"
-	];
+var cacheName = "cache_v3",
+    urlsToCache = [
+        "/",
+        "about-me",
+        "write-ups",
+        "search",
+        "index.json",
+        "404",
+        "site.webmanifest",
+        "images/profile.jpg",
+        "images/profile.webp"
+    ];
 
 self.addEventListener("install", function(event) {
-	event.waitUntil(caches.open(cacheName).then(function(cache) {
-		return cache.addAll([].concat.apply([], urlsToCache));
-	}).then(function() {
-		return self.skipWaiting();
-	}));
+    event.waitUntil(caches.open(cacheName).then(function(cache) {
+        return cache.addAll([].concat.apply([], urlsToCache));
+    }).then(function() {
+        return self.skipWaiting();
+    }));
 });
 
 self.addEventListener("fetch", function(event) {
-	if (!event.request.headers.get("Save-Data"))
-		event.respondWith(fetch(event.request).catch(function() {
-			return caches.match(event.request);
-		}));
+    if (!event.request.headers.get("Save-Data"))
+        event.respondWith(fetch(event.request).catch(function() {
+            return caches.match(event.request);
+        }));
 });
 
 self.addEventListener("activate", function(event) {
-	event.waitUntil(caches.keys().then(function(keyList) {
-		return Promise.all(keyList.map(function(key) {
-			if (cacheName.indexOf(key) === -1)
-				return caches.delete(key);
-			})
-		).then(function() {
-			return self.clients.claim();
-		});
-	}));
+    event.waitUntil(caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+            if (cacheName.indexOf(key) === -1)
+                return caches.delete(key);
+            })
+        ).then(function() {
+            return self.clients.claim();
+        });
+    }));
 });
