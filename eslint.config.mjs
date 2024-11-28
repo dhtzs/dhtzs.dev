9dbbeb8ba6819c81f611044ae6b922e3caf9f9ae
Dimitris Chatzis <12586834+dhtzs@users.noreply.github.com>
2024-11-28 22:39:13 +0200

    Minor fixes

---
+++ eslint.config.mjs
@@ -0,0 +1,58 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+
+import js from "@eslint/js";
+import css from "@eslint/css";
+import { tailwindSyntax } from "@eslint/css/syntax";
+import markdown from "@eslint/markdown";
+import json from "@eslint/json";
+
+export default defineConfig([
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: {
+        ...globals.node,
+        ...globals.builtin,
+        ...globals.amd,
+        ...globals.browser,
+        ...globals.serviceworker
+      }
+    }
+  },
+  {
+    plugins: { js },
+    files: ["**/*.js"],
+    extends: ["js/recommended"]
+  },
+  {
+    plugins: { css },
+    files: ["**/*.css"],
+    language: "css/css",
+    languageOptions: {
+      tolerant: true,
+      customSyntax: tailwindSyntax
+    },
+    extends: ["css/recommended"],
+    rules: {
+      "css/require-baseline": "off",
+      "css/no-invalid-properties": "off"
+    }
+  },
+  {
+    plugins: { json },
+    files: ["**/*.json"],
+    language: "json/json",
+    extends: ["json/recommended"]
+  },
+  {
+    plugins: { markdown },
+    files: ["**/*.md"],
+    language: "markdown/commonmark",
+    extends: ["markdown/recommended"],
+    rules: {
+      "markdown/no-missing-label-refs": "off"
+    }
+  },
+  globalIgnores(["public/", "resources/", "themes/", "package-lock.json", "backup/"])
+]);
