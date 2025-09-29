6fbe731c0d5785ead8cdfae31297ed98fef19fe8
Dimitris Chatzis <contact@dhtzs.dev>
2025-09-29 20:07:17 +0300

    Minor fixes

--- scripts/update.js
+++ scripts/update.js
@@ -1,11 +1,13 @@
-const { execSync } = require("child_process");
+import { execSync } from "child_process";
 
 try {
   console.log("-> Updating dependencies …");
   execSync("ncu -u --install always", { stdio: "inherit" });
 
-  console.log("-> Updating submodules …");
+  console.log("\n-> Updating submodules …");
   execSync("git submodule update --remote --merge", { stdio: "inherit" });
+  console.log("\n\x1b[32m%s\x1b[0m", "-> Update process completed successfully!");
 } catch (error) {
-  console.error("Error during update process:", error.message);
+  console.error("\n\x1b[31m%s\x1b[0m", "Error during update process:", error.message);
+  process.exit(1);
 }
