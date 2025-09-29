0cdbf82196dd4649651e9c6fbf0f809a3a948368
Dimitris Chatzis <contact@dhtzs.dev>
2025-09-29 20:07:17 +0300

    Minor fixes

--- scripts/update.js
+++ scripts/update.js
@@ -4,7 +4,7 @@ try {
   console.log("-> Updating dependencies …");
   execSync("ncu -u --install always", { stdio: "inherit" });
 
-  console.log("-> Updating submodules …");
+  console.log("\n-> Updating submodules …");
   execSync("git submodule update --remote --merge", { stdio: "inherit" });
 } catch (error) {
   console.error("Error during update process:", error.message);
